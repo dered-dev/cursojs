3fa1e724dbbb736eaa45416b3a2eba87acf8add1
jorge luis camarillo cristobal <jorge@jorges-MacBook-Pro.local>
2020-09-22 17:04:06 -0500

    ADD results exercises

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -20,7 +20,7 @@ for(let index = 0; index < kodersNames.length ; index++){
 
 // eliminar el ultimo item del array
 console.log(kodersNames.pop())
-// kodersNames.length === 3
+// kodersNames.splice(-1,1)
 console.log(kodersNames)
 
 // eliminar el primer item del array
@@ -70,6 +70,54 @@ console.log(allKodersFiltered)
 
 // Funciones
 // arrow functions
+
+
+let unique__listkoder = []
+let duplicate__listkoder = []
+
+
+for(let i= 0; i < allKoders.length ;i++ ){
+    if(unique__listkoder.includes(allKoders[i]) ) {
+        if(duplicate__listkoder.indexOf(allKoders[i]) === -1){
+            duplicate__listkoder.push(allKoders[i])
+        }
+    }else{
+        if(unique__listkoder.includes(allKoders[i]) === false){
+            unique__listkoder.push(allKoders[i])
+        }
+    }
+}
+// imprimir el nuevo array
+console.log(duplicate__listkoder)
+
+// for / reduce
+// let koderAges = [19, 20, 30, 50]
+
+// console.log(koderAges[0] + koderAges[1] + koderAges[2] + koderAges[3])
+let totalAges = 0;
+for(let counter = 0; counter < koderAges.length; counter++) {
+    totalAges = totalAges +  koderAges[counter]
+}
+console.log(totalAges)
+
+const totalAgesReduce = koderAges.reduce(function(acumulador, current, index, arr){
+    return acumulador + current
+}, 0)
+
+console.log('total de edades',totalAgesReduce)
+console.log('Promedio de edades',totalAgesReduce / koderAges.length)
+
+// total
+// total de elementos
+// promedio = total / total elementos
+
+
+
+
+
+
+
+
 
 
 
